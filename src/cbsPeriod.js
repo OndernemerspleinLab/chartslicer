ac44c8dfd4d01fe9e97e557dc05e27961977a4cb
Isaac Andela <mail@isaacandela.nl>
2017-06-14 17:13:28 +0200

    rewroking cbsPeriod parser

--- src/cbsPeriod.js
+++ src/cbsPeriod.js
@@ -31,224 +31,48 @@ const createDate = ({
 
 // Convert to zero based
 const zeroBased = index => Number(index) - 1
-const regexMatcher = regex => ({ part1 }) => regex.test(part1)
-const stringMatcher = string => ({ part1 }) => string === part1
 
-const perDay = {
-  match: regexMatcher(/^\d\d$/),
-  type: 'Dagen',
-  parse: ({ year, part1, part2 }) => {
-    const startDate = createDate({
-      year,
-      month: zeroBased(part1),
-      day: part2,
-    })
-    const endDate = endOfDay(startDate)
-    return {
-      type: perDay.type,
-      startDate,
-      endDate,
-      format: () => formatDate(startDate, 'D MMM YYYY'),
-    }
-  },
-}
-const perYear = {
-  match: stringMatcher('JJ'),
-  type: 'Jaar',
-  parse: ({ year }) => {
-    const startDate = createDate({ year })
-    const endDate = endOfYear(startDate)
-    return {
-      type: perYear.type,
-      startDate,
-      endDate,
-      format: () => formatDate(startDate, 'YYYY'),
-    }
-  },
-}
-const perMonth = {
-  match: stringMatcher('MM'),
-  type: 'Maanden',
-  parse: ({ year, part2 }) => {
-    const startDate = createDate({ year, month: zeroBased(part2) })
-    const endDate = endOfMonth(startDate)
-    return {
-      type: perMonth.type,
-      startDate,
-      endDate,
-      format: () => formatDate(startDate, 'MMM YYYY'),
-    }
-  },
-}
+const cbsPeriodMatcher = ({ type, indicator }) => ({ part1 }) =>
+  indicator === part1 ? type : undefined
 
-const perQuarter = {
-  match: stringMatcher('KW'),
+const perYearMatcher = cbsPeriodMatcher({ type: 'Jaar', indicator: 'JJ' })
+const perMonthMatcher = cbsPeriodMatcher({ type: 'Maanden', indicator: 'MM' })
+const perQuarterMatcher = cbsPeriodMatcher({
   type: 'Kwartalen',
-  parse: ({ year, part2 }) => {
-    const startDate = createDate({ year, month: zeroBased(part2) * 3 })
-    const endDate = endOfMonth(addMonths(startDate, 2)) // End of month adds the third month of the quarter
-    return {
-      type: perQuarter.type,
-      startDate,
-      endDate,
-      format: () => formatDate(startDate, 'YYYY [Q]Q'),
-    }
-  },
-}
+  indicator: 'KW',
+})
 
-const perSchoolYear = {
-  match: stringMatcher('SJ'),
-  type: 'School-, Bouw-, Oogstjaar',
-  parse: ({ year }) => {
-    const startDate = createDate({ year, month: 7, day: 1 })
-    const endDate = endOfMonth(addYears(createDate({ year, month: 6 }), 1))
-    return {
-      type: perSchoolYear.type,
-      startDate,
-      endDate,
-      format: () =>
-        `${formatDate(startDate, 'YYYY')}/${formatDate(endDate, 'YYYY')}`,
-    }
-  },
-}
-const perHalfYear = {
-  match: stringMatcher('HJ'),
-  type: 'Half jaar',
-  parse: ({ year, part2 }) => {
-    const startMonth = zeroBased(part2) * 6
-    const startDate = createDate({ year, month: startMonth })
-    const endDate = endOfMonth(addMonths(startDate, 6))
-
-    return {
-      type: perHalfYear.type,
-      startDate,
-      endDate,
-      format: () =>
-        `${formatDate(startDate, 'YYYY')} ${part2 === '01'
-          ? 'eerste helft'
-          : 'tweede helft'}`,
-    }
-  },
-}
-const perWeek = {
-  match: stringMatcher('W1'),
-  parse: ({ year, part2 }) => {
-    throw new Error('cbsPeriod type “Week, systeem 1” is not yet implemented')
-  },
-}
-const perFourWeeks = {
-  match: stringMatcher('W4'),
-  type: 'Week, vier weken',
-  parse: ({ year, part2 }) => {
-    throw new Error('cbsPeriod type “Week, vier weken” is not yet implemented')
-  },
-}
-const perRollingMonth = {
-  match: stringMatcher('VS'),
-  type: 'Voorschijdende maanden',
-  parse: ({ year, part2 }) => {
-    throw new Error(
-      'cbsPeriod type “Voorschijdende maanden” is not yet implemented'
-    )
-  },
-}
-const perTwoYearAverage = {
-  match: stringMatcher('G2'),
-  type: '2-jaarsgemiddelde',
-  parse: ({ year, part2 }) => {
-    throw new Error('cbsPeriod type “2-jaarsgemiddelde” is not yet implemented')
-  },
-}
-const perThreeYearAverage = {
-  match: stringMatcher('G3'),
-  type: '3-jaarsgemiddelde',
-  parse: ({ year, part2 }) => {
-    throw new Error('cbsPeriod type “3-jaarsgemiddelde” is not yet implemented')
-  },
-}
-const perFourYearAverage = {
-  match: stringMatcher('G4'),
-  type: '4-jaarsgemiddelde',
-  parse: ({ year, part2 }) => {
-    throw new Error('cbsPeriod type “4-jaarsgemiddelde” is not yet implemented')
-  },
-}
-const perFiveYearAverage = {
-  match: stringMatcher('G5'),
-  type: '5-jaarsgemiddelde',
-  parse: ({ year, part2 }) => {
-    throw new Error('cbsPeriod type “5-jaarsgemiddelde” is not yet implemented')
-  },
-}
-const perUnofficialPeriod = {
-  match: regexMatcher(/^X.$/),
-  type: 'Geen officiële periode indeling',
-  parse: ({ year, part2 }) => {
-    throw new Error(
-      'cbsPeriod type “Geen officiële periode indeling” is not yet implemented'
-    )
-  },
-}
-const perRollingYear = {
-  match: stringMatcher('VJ'),
-  type: 'Voortschrijdend jaar',
-  parse: ({ year, part2 }) => {
-    throw new Error(
-      'cbsPeriod type “Voortschrijdend jaar” is not yet implemented'
-    )
-  },
-}
-const perThreeMonthAverage = {
-  match: stringMatcher('M3'),
-  type: '3 maandelijks gemiddelde',
-  parse: ({ year, part2 }) => {
-    throw new Error(
-      'cbsPeriod type “3 maandelijks gemiddelde” is not yet implemented'
-    )
-  },
-}
-
-// All supported cbsPeriods
-const cbsPeriodTypes = [
-  perDay,
-  perYear,
-  perMonth,
-  perQuarter,
-  perSchoolYear,
-  perHalfYear,
-  perWeek,
-  perFourWeeks,
-  perRollingMonth,
-  perTwoYearAverage,
-  perThreeYearAverage,
-  perFourYearAverage,
-  perFiveYearAverage,
-  perUnofficialPeriod,
-  perRollingYear,
-  perThreeMonthAverage,
-]
+const periodMatchers = [perYearMatcher, perMonthMatcher, perQuarterMatcher]
 
 export const getCbsPeriodType = cbsPeriodString => {
   const cbsPeriod = splitCbsPeriodString(cbsPeriodString)
 
-  const { type } = cbsPeriodTypes.find(({ match }) => match(cbsPeriod)) || {}
+  for (let matcher of periodMatchers) {
+    const match = matcher(cbsPeriod)
 
-  return type
+    if (match) {
+      return match
+    }
+  }
 }
-export const parseCbsPeriod = cbsPeriodString => {
+
+const cbsPeriodToDateConverters = {
+  Jaar: ({ year }) => createDate({ year }),
+  Maanden: ({ year, part2 }) => createDate({ year, month: zeroBased(part2) }),
+  Kwartalen: ({ year, part2 }) =>
+    createDate({ year, month: zeroBased(part2) * 3 }),
+}
+
+export const convertCbsPeriodToDate = type => cbsPeriodString => {
   const cbsPeriod = splitCbsPeriodString(cbsPeriodString)
 
-  const { parse } = cbsPeriodTypes.find(({ match }) => match(cbsPeriod))
-
-  return parse(cbsPeriod)
+  return cbsPeriodToDateConverters[type](cbsPeriod)
 }
 
-export const getParserForType = parserType =>
-  cbsPeriodTypes.find(({ type }) => type === parserType)
-
-const createFormatPeriod = parser => cbsPeriodString => {
-  const cbsPeriod = splitCbsPeriodString(cbsPeriodString)
-  const { format } = parser(cbsPeriod)
-
-  return format()
+const cbsDataFormatters = {
+  Jaar: date => formatDate(date, 'YYYY'),
+  Maanden: date => formatDate(date, 'MMM YYYY'),
+  Kwartalen: date => formatDate(date, 'YYYY [Q]Q'),
 }
+
+export const formatCbsDate = type => cbsDate => cbsDataFormatters[type](cbsDate)
